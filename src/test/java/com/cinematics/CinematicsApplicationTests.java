a4fb43325f9d8a27f6a45e7b14167bbb8319a0d6
Momchil Asenov <momchil276@gmail.com>
2022-10-15 09:52:44 +0300

    Initial commit

---
+++ src/test/java/com/cinematics/CinematicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinematics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
